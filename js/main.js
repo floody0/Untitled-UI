b822f14fbc72b85bb9eec01795af47c61104e4e6
Ilya <ilyasemenenko91@gmail.com>
2024-02-03 22:14:18 +0200

    v1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+document.querySelector('.header__burger').addEventListener('click', function(){
+    this.classList.toggle('active');
+    document.querySelector('.navigation__list').classList.toggle('open');
+    document.querySelector('.header__authorization').classList.toggle('open');
+})
 window.onload = function() {
     const teamList = document.querySelectorAll('.team-section__person')
     teamList.forEach(function(item){
